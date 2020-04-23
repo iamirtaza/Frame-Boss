1b141d2a58241c90944fada615d73e4c94ab8d67
Syed Irtaza Mubashar <syedirtazamubashar@gmail.com>
2020-04-23 13:18:22 +0500

    Minor Changes

--- app/src/Components/Footer/index.js
+++ app/src/Components/Footer/index.js
@@ -12,57 +12,65 @@ class Footer extends React.Component {
             <div className="footer">
                 <Row className="footer-container">
                     <Col className="footer-section" lg={8} md={8} xs={24} sm={24}>
-                        <p>Get The App</p>
+                        <div style={{ display: 'flex' }}>
+                            <p>Get The App</p>
+                            <a>
+                                <img
+                                    style={{ marginLeft: '10px' }}
+                                    className="social-icon-facebook"
+                                    src={require('../../Assets/Icons/apple.svg')} />
+                            </a>
+                        </div>
+
                         <p>Follow Us</p>
                         <div >
-                            <img className="social-icon-facebook" src={require('../../Assets/Icons/facebook-letter-logo.svg')} />
-                            <img className="social-icons" src={require('../../Assets/Icons/twitter-black-shape.svg')} />
-                            <img className="social-icons" src={require('../../Assets/Icons/insta-icon.svg')} />
+                            <a><img className="social-icon-facebook" src={require('../../Assets/Icons/facebook-letter-logo.svg')} /></a>
+                            <a><img className="social-icons" src={require('../../Assets/Icons/twitter-black-shape.svg')} /></a>
+                            <a><img className="social-icons" src={require('../../Assets/Icons/insta-icon.svg')} /></a>
                         </div>
                         <br /><br />
                         <p>Get Inspiration</p>
                         <div className="get-inspiration" >
-                        <Input placeholder="Enter Your Email" />
-                        <Button>Submit</Button>
+                            <Input placeholder="Enter Your Email" />
+                            <Button>Submit</Button>
                         </div>
                     </Col>
                     <Col className="footer-section" lg={3} md={6} xs={12} sm={12}>
-                        <p>How It Works</p>
-                        <p>Press</p>
-                        <p>For Art & Trade</p>
-                        <p>The Shop</p>
+                        <a><p>How It Works</p></a>
+                        <a><p>Press</p></a>
+                        <a><p>For Art & Trade</p></a>
+                        <a><p>The Shop</p></a>
                     </Col>
                     <Col className="footer-section" lg={3} md={6} xs={12} sm={12}>
-                        <p>FAQ</p>
-                        <p>For Buisness</p>
-                        <p>Company</p>
-                        <p>Careers</p>
+                        <a><p>FAQ</p></a>
+                        <a><p>For Buisness</p></a>
+                        <a><p>Company</p></a>
+                        <a><p>Careers</p></a>
                     </Col>
                     <Col className="footer-section" lg={3} md={4} xs={12} sm={12}>
-                        <p>Stores</p>
-                        <p>Pricing</p>
-                        <p>Reviews</p>
-                        <p>Design Services</p>
+                        <a><p>Stores</p></a>
+                        <a><p>Pricing</p></a>
+                        <a><p>Reviews</p></a>
+                        <a><p>Design Services</p></a>
                     </Col>
                     <Col className="footer-section" lg={3} md={6} xs={12} sm={12}>
-                        <p>About Us</p>
-                        <p>Gift Card</p>
-                        <p>Contact Us</p>
+                        <a><p>About Us</p></a>
+                        <a><p>Gift Card</p></a>
+                        <a><p>Contact Us</p></a>
                     </Col>
                     <Col className="footer-section" lg={3} md={6} xs={12} sm={12}>
-                        <p>Terms Of Service</p>
-                        <p>Privacy Policy</p>
-                        <p>Accessibility</p>
-                        <p>Sitemap</p>
+                        <a><p>Terms Of Service</p></a>
+                        <a><p>Privacy Policy</p></a>
+                        <a><p>Accessibility</p></a>
+                        <a><p>Sitemap</p></a>
                     </Col>
                 </Row>
-                <br/>
-               <Row className="footer-credits" >
-               
-               <Col span={24}>
-               <center><p>© 2020 index.</p></center>
+                <br />
+                <Row className="footer-credits" >
+                    <Col span={24}>
+                        <center><p>© 2020 index.</p></center>
                     </Col>
-               </Row>
+                </Row>
             </div>
         )
     }
